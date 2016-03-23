166c7d64e093f5e9860f96f5fba77fcc83cb570d
Ross Harrison <rtharrison86@gmail.com>
2016-03-22 20:02:41 -0400

    use actual movements to shuffle the board

--- www/js/app.js
+++ www/js/app.js
@@ -65,7 +65,7 @@
 
   var HEIGHT = 5;
   var WIDTH = 3;
-  var HIDE_MENU_TIMEOUT = 2000;
+  var HIDE_MENU_TIMEOUT = 7000;
 
   /*
    * master directive
@@ -112,7 +112,7 @@
     $scope.resetClick = function(){
       var confirmPopup = $ionicPopup.confirm({
         title: 'Start A new Game',
-        template: 'Are you sure you want to start a new game?'
+          template: 'Are you sure you want to start a new game?'
       });
       confirmPopup.then(function(res) {
         if(res) {
@@ -147,9 +147,9 @@
   app.directive("gameGrid", function(){
     return {
       restrict: "A",
-      require: "master",
-      controller: "GameGridController",
-      controllerAs: "gridCtrl",
+    require: "master",
+    controller: "GameGridController",
+    controllerAs: "gridCtrl",
     };
   });
 
@@ -178,7 +178,7 @@
       "splash.jpg",
       "snow-street.jpg",
       "helix.jpg",
-    ];
+      ];
 
     /*
      * start the game over
@@ -236,20 +236,38 @@
      * shuffle the grid squares
      */
     this.shuffle = function(){
-      gridSquares = _.shuffle(gridSquares);
-      for(var i = 0; i < WIDTH; i++){
-        gridSquares[i] = _.shuffle(gridSquares[i]);
-      }
+      var shuffles = Math.floor(Math.random() * 20) + 20;
+      var row, col;
 
-      for(var i = 0; i < WIDTH; i++){
-        for(var j = 0; j < HEIGHT; j++){
-          gridSquares[i][j].live.setPosition(i, j);
+      for(var i = 0; i < shuffles; i++){
+        switch(Math.floor(Math.random() * 4)){
+          case 0:
+            // slide right 
+            row = Math.floor(Math.random() * HEIGHT);
+            slideRow(row, "right");
+            break;
+          case 1:
+            // slide left
+            row = Math.floor(Math.random() * HEIGHT);
+            slideRow(row, "left");
+            break;
+          case 2:
+            // slide up 
+            col = Math.floor(Math.random() * WIDTH);
+            slideColumn(col, "up");
+            break;
+          case 3:
+            //slide down
+            col = Math.floor(Math.random() * WIDTH);
+            slideColumn(col, "down");
+            break;
         }
       }
+
       _this.shuffled = true;
       _this.sliding = true;
     };
-   
+
     /*
      * shuffle board if not done so yet
      */ 
@@ -269,7 +287,8 @@
       sp.setPosition(prepPos.x, prepPos.y);
       $timeout(function(){
         sp.setSpare(false);
-      })
+        $element.addClass("sliding");
+      });
     }
 
     /*
@@ -291,6 +310,7 @@
     function slideRow(idx, dir){
       var isLeft = dir === 'left';
       var adjust = (isLeft) ? -1 : 1;
+      var i;
 
       /*
        * slide the spare
@@ -305,7 +325,7 @@
       /*
        * slide other cells
        */
-      for(var i = 0; i < WIDTH; i++){
+      for(i = 0; i < WIDTH; i++){
         gridSquares[i][idx].live.setPosition(i + adjust, idx);
       }
 
@@ -313,26 +333,26 @@
        * adjust column
        */
       var spareSqr = null;
+      var wrapSquare;
+
       if(isLeft){
-        var wrapSquare = gridSquares[0][idx];
-        for(var i = 1; i < WIDTH; i++){
+        wrapSquare = gridSquares[0][idx];
+        for(i = 1; i < WIDTH; i++){
           gridSquares[i-1][idx] = gridSquares[i][idx];
         }
         gridSquares[WIDTH-1][idx] = wrapSquare;
       }
       else{
-        var wrapSquare = gridSquares[WIDTH-1][idx];
-        for(var i = WIDTH-2; i >= 0; i--){
+        wrapSquare = gridSquares[WIDTH-1][idx];
+        for(i = WIDTH-2; i >= 0; i--){
           gridSquares[i+1][idx] = gridSquares[i][idx];
         }
         gridSquares[0][idx] = wrapSquare;
       }
 
       // switch spares  for slider 
-      $timeout(function(){
-        var col = (isLeft) ? WIDTH - 1 : 0;
-        switchSpare({x: col, y: idx});
-      }, 250);
+      var col = (isLeft) ? WIDTH - 1 : 0;
+      switchSpare({x: col, y: idx});
     }
 
     /*
@@ -363,20 +383,19 @@
        * adjust column
        */
       var spareSqr = null;
+      var wrapSquare;
       if(isUp){
-        var wrapSquare = gridSquares[idx].shift();
+        wrapSquare = gridSquares[idx].shift();
         gridSquares[idx].push(wrapSquare);
       }
       else{
-        var wrapSquare = gridSquares[idx].pop();
+        wrapSquare = gridSquares[idx].pop();
         gridSquares[idx].unshift(wrapSquare);
       }
 
       // switch spares  for slider 
-      $timeout(function(){
-        var row = (isUp) ? HEIGHT - 1 : 0;
-        switchSpare({x: idx, y: row});
-      }, 250);
+      var row = (isUp) ? HEIGHT - 1 : 0;
+      switchSpare({x: idx, y: row});
     }
 
     /*
@@ -433,6 +452,8 @@
     });
 
     $element.on("transitionend", function(){
+      $element.removeClass("sliding");
+
       if(!_this.sliding) return;
       _this.sliding = false;
       checkVictory();
@@ -470,7 +491,7 @@
           gridSquares[i][j].live.setPosition(p.x, p.y);
         }
       }
-    };
+    }
   }
   app.controller("GameGridController", GameGridController);
 
@@ -481,19 +502,19 @@
   app.directive("gridSquare", function(){
     return {
       restrict: "A",
-      require: ["^^gameGrid", "gridSquare"],
-      controller: "GridSquareController",
-      controllerAs: "sqrCtrl",
-      template: "<div class='grid-style'></div>",
-      scope: {
-        "x": "@",
-        "y": "@"
-      },
-      link: function($scope, $element, attrs, requirements){
-        var gameGrid = requirements[0];
-        var gridSquare = requirements[1];
-        gameGrid.registerSquare(gridSquare);
-      }
+    require: ["^^gameGrid", "gridSquare"],
+    controller: "GridSquareController",
+    controllerAs: "sqrCtrl",
+    template: "<div class='grid-style'></div>",
+    scope: {
+      "x": "@",
+    "y": "@"
+    },
+    link: function($scope, $element, attrs, requirements){
+            var gameGrid = requirements[0];
+            var gridSquare = requirements[1];
+            gameGrid.registerSquare(gridSquare);
+          }
     };
   });
 
@@ -533,16 +554,27 @@
     /*
      * set 'physical position'
      */
-    this.setPosition = function(x, y){
+    this.setPosition = function(x, y, now){
       posX = x;
       posY = y;
+      if(now)
+        refreshTransform();
+      else
+        bounceTransform();
+    };
+
+    /*
+     * set the transform
+     */
+    var refreshTransform = function(tX, tY){
       var tX = posX * 100;
       var tY = posY * 100;
-
       $element.css({
         "transform": "translate3d("+tX+"%, "+tY+"%, 0)",
       });
     };
+
+    var bounceTransform = _.debounce(refreshTransform);
 
     /*
      * setSpare class on grid
@@ -576,48 +608,47 @@
     this.getActualPosition = function(){
       return {
         x: parseInt($scope.x),
-        y: parseInt($scope.y)
+          y: parseInt($scope.y)
       };
+    };
 
-    }
 
+    var H_CENTER = Math.floor(WIDTH / 2);
+    var V_CENTER = Math.floor(HEIGHT/ 2);
 
-    var H_CENTER = Math.floor(WIDTH / 2)
-      var V_CENTER = Math.floor(HEIGHT/ 2)
+    /*
+     * Initialize the background image and position
+     */ 
+    function initBackgroundImage(){
+      var blockHeight = window.innerHeight / HEIGHT;
+      var blockWidth = window.innerWidth / WIDTH;
+      var backX = ($scope.x - H_CENTER) * blockWidth;
+      var backXStr = "50%";
+      var backY = ($scope.y - V_CENTER) * blockHeight;
+      var backYStr = "50%";
 
-      /*
-       * Initialize the background image and position
-       */ 
-      function initBackgroundImage(){
-        var blockHeight = window.innerHeight / HEIGHT;
-        var blockWidth = window.innerWidth / WIDTH;
-        var backX = ($scope.x - H_CENTER) * blockWidth;
-        var backXStr = "50%";
-        var backY = ($scope.y - V_CENTER) * blockHeight;
-        var backYStr = "50%";
-
-        if(backX < 0) {
-          backXStr = "calc(50% + "+Math.abs(backX)+"px)";
-        } else if (backX > 0){
-          backXStr = "calc(50% - "+backX+"px)";
-        }
-        if(backY < 0) {
-          backYStr = "calc(50% + "+Math.abs(backY)+"px)";
-        } else if (backY > 0){
-          backYStr = "calc(50% - "+backY+"px)";
-        }
-
-        $element.css({
-          "background-position": backXStr+" "+backYStr
-        });
+      if(backX < 0) {
+        backXStr = "calc(50% + "+Math.abs(backX)+"px)";
+      } else if (backX > 0){
+        backXStr = "calc(50% - "+backX+"px)";
       }
+      if(backY < 0) {
+        backYStr = "calc(50% + "+Math.abs(backY)+"px)";
+      } else if (backY > 0){
+        backYStr = "calc(50% - "+backY+"px)";
+      }
+
+      $element.css({
+        "background-position": backXStr+" "+backYStr
+      });
+    }
 
     /*
      * Initialize
      */
     $scope.x = parseInt($scope.x);
     $scope.y = parseInt($scope.y);
-    _this.setPosition($scope.x, $scope.y);
+    _this.setPosition($scope.x, $scope.y, true);
     initBackgroundImage();
     angular.element($window).on('resize', initBackgroundImage);
   }
